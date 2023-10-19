f12c2c6c15637bd17efb23d7f1cedcb123aa4a07
ltfreeuni <l.tskipuri@freeuni.edu.ge>
2023-10-19 14:32:22 +0400

    some minor modification

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>Learn & Master GitHub Actions V1.1!</h1>
+        <h1>Learn & Master GitHub Actions V1.1</h1>
       </header>
       <MainContent />
     </>
